6182862a27e379fc929dd0d988c161558256215d
Evgeniy Ermolov <47905389+7evgeniy@users.noreply.github.com>
2022-11-06 02:19:06 +0300

    добавлены средства к выводу через Gui

--- simplex.py
+++ simplex.py
@@ -8,13 +8,13 @@ sp.init_printing()
 # matrix : тип sp.Matrix + следующие атрибуты:
 #     .free : число: столбец, который должен войти в базис;
 #     .nonfree : число: столбец, который должен выйти из базиса.
-# freeList : список чисел: столбцы, входящие в базис.
+#     .freeList : список чисел: столбцы, входящие в базис.
 
 # войдёт в базис (столбец); а пока — вне базиса: → matrix.nonfree
-def compute_nonfree(matrix, freeList):
+def compute_nonfree(matrix):
 	minimum = 0
 	for j in range(1, matrix.cols):
-		if not (j in freeList) and matrix[0, j] < minimum:
+		if not (j in matrix.freeList) and matrix[0, j] < minimum:
 			matrix.nonfree = j
 			minimum = matrix[0, j]
 	return True if minimum else False
@@ -42,24 +42,35 @@ def update_matrix(matrix, free, nonfree):
 	print("out: {}, in: {}".format(free, nonfree))
 	sp.pprint(matrix); print(); print()
 
+
 # симплекс-метод расписан в этой процедуре.
-# на входе — симплексная таблица и список базисных столбцов
+# на входе — симплексная таблица со списком базисных столбцов
 # на выходе — симплексная таблица по результатам преобразований
-def simplex(matrix, freeList):
+def simplex(matrix):
 	sp.pprint(matrix); print(); print()
 	while True:
-		if not compute_nonfree(matrix, freeList):  # в базис войдёт (столбец)
+		if not compute_nonfree(matrix):            # в базис войдёт (столбец)
 			return matrix
 		if not compute_free(matrix):               # из базиса выйдет (строка)
 			return None
-		freeList[matrix.free-1] = matrix.nonfree
+		matrix.freeList[matrix.free-1] = matrix.nonfree
 		update_matrix(matrix, matrix.free, matrix.nonfree)
 
-def solve(rowList, height, width):
-	m = sp.zeros(height, width + height - 1)
-	for i in range(len(rowList)):
-		for j in range(len(rowList[i])):
-			m[i, j] = rowList[i][j]
-		if i > 0:
-			m[i, width+i-1] = 1
-	return simplex(m, list(range(width, width+height-1)))
+# способ вывода симплекс-таблицы matrix: через Gui.
+# процедура возвращает виджет w, показывающий симплекс-таблицу.
+
+from PyQt5 import QtWidgets
+import numpy as np
+
+def get_table(matrix, names):
+	r, c = matrix.rows, matrix.cols
+	w = QtWidgets.QTableWidget(r+1, c+1)
+	for i, j in np.ndindex(r, c):
+		w.setItem(i+1, j+1, QtWidgets.QTableWidgetItem(str(matrix[i, j])))
+	for j in range(c-1):
+		w.setItem(0, j+2, QtWidgets.QTableWidgetItem(names[j]))
+	for i in range(r-1):
+		w.setItem(i+2, 0, QtWidgets.QTableWidgetItem(names[matrix.freeList[i]-1]))
+	w.setItem(0, 1, QtWidgets.QTableWidgetItem('!'))
+	w.setItem(1, 0, QtWidgets.QTableWidgetItem('!'))
+	return w
